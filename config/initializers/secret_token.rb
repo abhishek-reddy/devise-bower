eb30d593e386b98604562490d511511a479b4e27
Abhishek Reddy <abhishekreddy.d@vasudhaika.net>
2014-05-07 12:11:21 +0530

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseBower::Application.config.secret_key_base = 'd2cfb987a06c87bf81eba57bd3e3ba6cf48811b86e25b129369582c3b60f81cd7f849da647d19d25a51623ec853b005371dd845dd0653eb98ee3b576226e8240'
